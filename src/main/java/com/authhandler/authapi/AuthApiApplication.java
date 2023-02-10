f19718bc6a11e3e21dcfd2f396229b44373d9298
jacoobanderson <j00cwa@gmail.com>
2023-02-10 16:29:19 +0100

    Add logger

--- src/main/java/com/authhandler/authapi/AuthApiApplication.java
+++ src/main/java/com/authhandler/authapi/AuthApiApplication.java
@@ -1,11 +1,17 @@
 package com.authhandler.authapi;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class AuthApiApplication {
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    	return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApiApplication.class, args);
--- src/main/java/com/authhandler/authapi/controller/UserController.java
+++ src/main/java/com/authhandler/authapi/controller/UserController.java
@@ -3,6 +3,7 @@ package com.authhandler.authapi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.authhandler.authapi.model.LoginRequest;
@@ -10,8 +11,9 @@ import com.authhandler.authapi.model.User;
 import com.authhandler.authapi.service.UserService;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
-  
+
   @Autowired
   private UserService userService;
 
